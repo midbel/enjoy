9edc4ff7d488b72206a7f04666a81866cd703b13
midbel <brun.nicolas21@gmail.com>
2023-09-27 21:20:21 +0200

    additional test cases

--- parser/testdata/export.js
+++ parser/testdata/export.js
@@ -4,8 +4,13 @@ export function foobar() {}
 
 export {foo, bar}
 export {foo as superfoo, bar}
+export default foobar
+export * from "foobar"
+export * as alias from "foobar"
 
+import foobar from "foobar"
 import * as foobar from "foobar"
 import {foo, bar} from "foobar"
 import {foo as superfoo, bar} from "foobar"
 import "foobar"
+import {default as alias} from "foobar"
