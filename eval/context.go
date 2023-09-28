2037ca79862b6b8c880996d9cf957ee43486705d
midbel <brun.nicolas21@gmail.com>
2023-09-28 20:51:36 +0200

    context type?

---
+++ eval/context.go
@@ -0,0 +1,27 @@
+package eval
+
+import (
+	"github.com/midbel/enjoy/env"
+	"github.com/midbel/enjoy/value"
+)
+
+type context struct {
+	env.Environ[value.Value]
+	modules map[string]Module
+}
+
+func defaultContext(ev env.Environ[value.Value]) *context {
+	return &context{
+		Environ: ev,
+		modules: make(map[string]Module)
+	}
+}
+
+type Module struct {
+
+}
+
+func Load() Module {
+	var mod Module
+	return mod
+}
